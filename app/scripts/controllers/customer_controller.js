45fb42a9483e3e405218571f118cb006b583b038
hazeltree <steven@hazeltree.org>
2014-06-25 07:17:42 -0700

    active customer logic

--- app/scripts/controllers/customer_controller.js
+++ app/scripts/controllers/customer_controller.js
@@ -1,14 +1,3 @@
 SgwAdmin.CustomerController = Ember.ObjectController.extend({
-  needs: ['customers'],
-  // Implement your controller here.
-  toggle: false,
-  actions: {
-    toggle: function() {
-      this.get('controllers.customers.content').forEach(function(item){
-        Ember.Logger.log(item.get('toggle'));
-        item.set('toggle', false);
-      });
-      this.set('toggle', true);
-    }
-  }
+  isActive: false
 });
--- app/scripts/controllers/customers_controller.js
+++ app/scripts/controllers/customers_controller.js
@@ -1,5 +1,31 @@
-SgwAdmin.CustomersController = Ember.ObjectController.extend({
+SgwAdmin.CustomersController = Ember.ArrayController.extend({
   // Implement your controller here.
-  hello: function(){}.property()
-});
+  itemController: 'customer',
+  activeItem: Ember.reduceComputed('@this.@each.isActive', {
+    initialValue: null,
 
+    removedItem: function (accumulatedValue, item, changeMeta, instanceMeta) {
+      if (item.get('isActive')) {
+        var previousActiveItem = instanceMeta.activeItem;
+        if (previousActiveItem) previousActiveItem.set('isActive', false);
+        return instanceMeta.activeItem = item;
+      }
+      return instanceMeta.activeItem = null;
+    },
+
+    addedItem: function (accumulatedValue, item, changeMeta, instanceMeta) {
+      return instanceMeta.activeItem;
+    }
+  }),
+ 
+  // Ensures the above add/remove callbacks are called
+  setActiveItem: function () {
+    this.get('activeItem');
+  }.observes('@each.isActive'),
+  
+  actions: {
+    toggleActive: function (controller) {
+      controller.toggleProperty('isActive');
+    }
+  }
+});
