7915e48dd63e84524c3be45aafa60d36f557afc6
Steven Valladolid <steven@hazeltree.org>
2014-07-28 17:29:35 -0700

    refactored templates

--- app/scripts/models/quote.js
+++ app/scripts/models/quote.js
@@ -8,7 +8,25 @@ SgwAdmin.Quote = DS.Model.extend({
   squareFeet: DS.attr('string'),
   squareFootPrice: DS.attr('number'),
   totalCost: DS.attr('number'),
-  temperedGass: DS.attr('string'),
+  temperedGlass: DS.attr('string'),
   framing: DS.attr('string'),
   misc: DS.attr('string'),
 });
+
+SgwAdmin.Quote.FIXTURES = [
+  {
+    id: 1,
+    sender: 1,
+    date: '06/01/14',
+    firstName: 'Jane',
+    email: 'jane@doe.com',
+    pattern: 'Abstract art',
+    size: '45 x 45',
+    squareFeet: '14',
+    squareFootPrice: 90,
+    totalCost: 1460,
+    temperedGlass: 'none',
+    framing: 'Standard brace bar for minimal install',
+    misc: 'some additional notes'
+  }
+];
--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,5 +1,12 @@
 SgwAdmin.Router.map(function () {
   this.resource('clients');
-  this.resource('orders');
-  this.resource('quotes');
+  this.resource('orders', function(){
+  	this.resource('order', { path: '/:order_id' });
+  });
+  this.resource('quotes', function(){
+  	this.resource('quote', { path: '/:quote_id' });
+  });
+  this.resource('requests', function(){
+  	this.resource('request', { path: '/:request_id' });
+  });
 });
